9102e15c00fa04181bf4097f68d99a55d191318f
Stefen Sutandi <stefensutandi@gmail.com>
2023-11-23 08:39:11 +0700

    ascii_bcd.vhd

--- ascii_bcd.vhd
+++ ascii_bcd.vhd
@@ -16,21 +16,21 @@ architecture behavioral of ascii_bcd is
         process(ascii_x_input, ascii_y_input)
         begin
             -- ASCII to BCD Conversion for x
-            variable x_digit: integer;
-            x_digit := to_integer(unsigned(ascii_x_input));
+            variable x_digit: unsigned(3 downto 0);
+            x_digit := unsigned(ascii_x_input);
             case x_digit is
-                when 48 to 57 =>  -- ASCII values for '0' to '9'
-                    bcd_x_output <= std_logic_vector(to_unsigned(x_digit - 48, 4));  -- Convert ASCII to BCD
+                when "0011" to "1001" =>  -- ASCII values for '0' to '9'
+                    bcd_x_output <= std_logic_vector(x_digit - "0011");  -- Convert ASCII to BCD
                 when others =>
                     bcd_x_output <= "0000";  -- Output '0000' for invalid ASCII characters
             end case;
         
             -- ASCII to BCD Conversion for y
-            variable y_digit: integer;
-            y_digit := to_integer(unsigned(ascii_y_input));
+            variable y_digit: unsigned(3 downto 0);
+            y_digit := unsigned(ascii_y_input);
             case y_digit is
-                when 48 to 57 =>  -- ASCII values for '0' to '9'
-                    bcd_y_output <= std_logic_vector(to_unsigned(y_digit - 48, 4));  -- Convert ASCII to BCD
+                when "0011" to "1001" =>  -- ASCII values for '0' to '9'
+                    bcd_y_output <= std_logic_vector(y_digit - "0011");  -- Convert ASCII to BCD
                 when others =>
                     bcd_y_output <= "0000";  -- Output '0000' for invalid ASCII characters
             end case;
