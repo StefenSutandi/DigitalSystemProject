09c54fe222af95ae65c4b5ada5cb00aff4ffa599
Osamah Azhar <osamahazhar18@gmail.com>
2023-11-25 02:11:19 +0700

    multiplier biner

--- multiplierbin.vhd
+++ multiplierbin.vhd
@@ -1,29 +1,40 @@
-library IEEE; 
-use IEEE.std_logic_1164.all; 
-use IEEE.std_logic_unsigned.all; 
-use IEEE.numeric_std.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
+
+-- dua 40-bit input and one 8-bit outputs
+-- Dua input 40-bit (12 digit) dan output 40 bit (12 digit)
 entity multiplierbin is
-    port (
-        x_bin: in std_logic_vector(4 downto 0);
-        y_bin: in std_logic_vector(4 downto 0);
-        multiplier_out: out std_logic_vector(9 downto 0)
-    );
-end entity multiplierbin;
+port(	x, y:	in std_logic_vector(39 downto 0);
+	    hasil: 	out std_logic_vector(39 downto 0)
+);
+end multiplierbin;
 
 architecture behavioral of multiplierbin is
-    signal temp_multi: std_logic_vector(9 downto 0);
-    constant y: integer;
-begin
-process(x_bin, y_bin)
 
 begin
-y <= to_integer(unsigned(y_bin));
-    for i in 0 to y loop
-        temp_multi <= temp_multi + x_bin ;
-        end loop;
+process(x, y)
+	
+  variable x_reg: std_logic_vector(40 downto 0);
+  variable hasil_reg: std_logic_vector(81 downto 0);
+begin	 
+	
+  x_reg := "0" & x;
+  hasil_reg := "000000000000000000000000000000000000000000" & y;
 
-multiplier_out <= temp_multi;
+  for i in 1 to 41 loop
+    if hasil_reg(0)='1' then
+	  hasil_reg(81 downto 41) := hasil_reg(81 downto 41) 
+	  + x_reg(40 downto 0);
+	end if;
+	hasil_reg(81 downto 0) := '0' & hasil_reg(81 downto 1);
+  end loop;
+  
+  -- Output
+  hasil <= hasil_reg(39 downto 0);
 
-    end process;
-end architecture behavioral;
+end process;
+
+end behavioral;
