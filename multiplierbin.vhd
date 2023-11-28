fbd17abe1ff160a57350d5f15f140b29b883a517
Osamah Azhar <osamahazhar18@gmail.com>
2023-11-28 14:35:18 +0700

    multiplier diubah ke 4 digit

--- multiplierbin.vhd
+++ multiplierbin.vhd
@@ -4,11 +4,10 @@ use IEEE.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 
--- dua 40-bit input and one 8-bit outputs
--- Dua input 40-bit (12 digit) dan output 40 bit (12 digit)
+-- Dua input 14-bit (4 digit) dan output 14 bit (4 digit)
 entity multiplierbin is
-port(	x, y:	in std_logic_vector(39 downto 0);
-	    hasil: 	out std_logic_vector(39 downto 0)
+port(	x, y:	in std_logic_vector(13 downto 0);
+	    hasil: 	out std_logic_vector(13 downto 0)
 );
 end multiplierbin;
 
@@ -17,23 +16,23 @@ architecture behavioral of multiplierbin is
 begin
 process(x, y)
 	
-  variable x_reg: std_logic_vector(40 downto 0);
-  variable hasil_reg: std_logic_vector(81 downto 0);
+  variable x_reg: std_logic_vector(14 downto 0);
+  variable hasil_reg: std_logic_vector(27 downto 0);
 begin	 
 	
   x_reg := "0" & x;
-  hasil_reg := "000000000000000000000000000000000000000000" & y;
+  hasil_reg := "00000000000000" & y;
 
-  for i in 1 to 41 loop
+  for i in 1 to 14 loop
     if hasil_reg(0)='1' then
-	  hasil_reg(81 downto 41) := hasil_reg(81 downto 41) 
-	  + x_reg(40 downto 0);
+	  hasil_reg(27 downto 14) := hasil_reg(27 downto 14) 
+	  + x_reg(13 downto 0);
 	end if;
-	hasil_reg(81 downto 0) := '0' & hasil_reg(81 downto 1);
+	hasil_reg(27 downto 0) := '0' & hasil_reg(27 downto 1);
   end loop;
   
   -- Output
-  hasil <= hasil_reg(39 downto 0);
+  hasil <= hasil_reg(13 downto 0);
 
 end process;
 
