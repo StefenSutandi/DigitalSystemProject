d705bdae404c54425c19975360503c9a8e8ab5b3
unknown <creojr88@gmail.com>
2023-12-05 10:33:28 +0700

    adder

--- adder.vhd
+++ adder.vhd
@@ -1,91 +1,100 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 entity adder is
-    port (
-        x: in std_logic_vector(15 downto 0);
-        y: in std_logic_vector(15 downto 0);
-        carry_out: out std_logic;
-        sum_bcd: out std_logic_vector(15 downto 0);
-        error_flag: out std_logic
-    );
-end entity adder;
+  port (
+    Clk, reset_count, Load : in  std_logic;
+    A, B              : in  std_logic_vector(13 downto 0);
+    Ready             : out std_logic;
+    Sum               : out std_logic_vector(13 downto 0)
+  );
+end entity;
 
 architecture behavioral of adder is
-    signal x_bcd, y_bcd: std_logic_vector(15 downto 0);
-    signal sum_bcd1: std_logic_vector(15 downto 0);
-    signal carry: std_logic := '0';  
-    signal temp_carry: std_logic_vector(3 downto 0) := (others => '0');  -- Signal utk carry sementara
-    constant BCD_group : integer := 4;
+  type states is (Idle, S_XOR, S_XNOR);
+  signal state, state_next : states;
 
-component ascii_bcd is
-    port(
-        ascii_x_input: in std_logic_vector(7 downto 0); -- Input x (ASCII)
-        ascii_y_input: in std_logic_vector(7 downto 0); -- Input y (ASCII)
-        bcd_x_output: out std_logic_vector(3 downto 0); -- Output BCD  x
-        bcd_y_output: out std_logic_vector(3 downto 0)  -- Output BCD  y
-    );
-end component;   
+  signal Ashift, Ashift_next   : std_logic_vector(13 downto 0);
+  signal Bshift, Bshift_next   : std_logic_vector(13 downto 0);
+  signal SumShift, SumShift_next : std_logic_vector(13 downto 0);
 
-component bcd_ascii is
-    port (
-        bcd_x_input: in std_logic_vector(7 downto 0); -- Input BCD (12-digit x 4-bit)
-        bcd_y_input: in std_logic_vector(7 downto 0); -- Input BCD (12-digit x 4-bit)
-        ascii_x_output: out std_logic_vector(3 downto 0); -- Output ASCII (48-bit)
-        ascii_y_output: out std_logic_vector(3 downto 0) -- Output ASCII (48-bit)
-    );
-end component;
-
+  signal count, next_count : integer range 0 to 14;
+  signal Carry         : std_logic;
 begin
-    -- Konversi ASCII ke BCD untuk X dan Y
-    bcd_conversion: ascii_bcd
-        port map (
-            ascii_x_input => x,
-            bcd_x_output => x_bcd,
-            ascii_y_input => y,
-            bcd_y_output => y_bcd
-        ); 
+  sequential: process (clk, reset_count) is
+  begin
+    if clk'event and clk = '1' then
+      if reset_count = '1' then
+        state <= Idle;
+        Ashift <= (others => '0');
+        Bshift <= (others => '0');
+        SumShift <= (others => '0');
+        count <= 0;
+      else
+        state <= state_next;
+        Ashift <= Ashift_next;
+        Bshift <= Bshift_next;
+        SumShift <= SumShift_next;
+        count <= next_count;
+      end if;
+    end if;
+  end process;
 
-    -- Adder dalam BCD
-    process(x_bcd, y_bcd)
-        variable temp_sum: integer;
-        variable temp_result: std_logic_vector(15 downto 0);
-    begin
-	temp_carry <= std_logic_vector(to_unsigned(0, temp_carry'length)); -- Initialize temp_carry
+  combinational: process (state, Ashift, Bshift, SumShift, count, next_count, Load, A, B) is
+    variable Carry: std_logic;
+  begin
+    Ashift_next <= Ashift;
+    Bshift_next <= Bshift;
+    SumShift_next <= SumShift;
+    next_count <= count;
+    ready <= '0';
 
-        -- Loop per 4 BCD bit dari LSB ke MSB
-        for i in 3 downto 0 loop
-		temp_sum := to_integer(unsigned(x_bcd(i * BCD_group + BCD_group - 1 downto i * BCD_group))) +
-					to_integer(unsigned(y_bcd(i * BCD_group + BCD_group - 1 downto i * BCD_group))) +
-					to_integer(unsigned(temp_carry));
-
-            if temp_sum < 10 then
-                temp_result(i * BCD_group + BCD_group - 1 downto i * BCD_group) := std_logic_vector(to_unsigned(temp_sum, BCD_group));
-                temp_carry <= std_logic_vector(to_unsigned(0, temp_carry'length));
-
-            else
-                temp_result(i * BCD_group + BCD_group - 1 downto i * BCD_group) := std_logic_vector(to_unsigned(temp_sum + 6, BCD_group));
-                temp_carry <= std_logic_vector(to_unsigned(1, temp_carry'length));
-
-            end if;
-        end loop;
-
-        -- Hasil masuk ke signal sum_bcd
-        sum_bcd <= temp_result;
-
-        -- Error case untuk hasil lebih besar dari 16 bit
-        if sum_bcd'length > 16 then
-            error_flag <= '1';
+    case state is
+      when Idle =>
+        ready <= '1';
+        if Load = '1' then
+          Ashift_next <= A;
+          Bshift_next <= B;
+          SumShift_next <= (others => '0');
+          next_count <= 14;
+          state_next <= S_XOR;
         else
-            error_flag <= '0';
+          state_next <= Idle;
         end if;
-    end process;
+      when S_XOR =>
+        Carry := Ashift(0) xor Bshift(0);
 
-    -- Konversi BCD ke ASCII untuk sum_bcd
-	sum_ascii_conversion: bcd_ascii
-		port map (
-			bcd_input => sum_bcd,
-			ascii_output => sum_ascii
-		);
-end architecture behavioral;
+        Ashift_next <= Ashift(13) & Ashift(13 downto 1);
+        Bshift_next <= Bshift(13) & Bshift(13 downto 1);
+        SumShift_next <= Carry & SumShift(13 downto 1);
+        next_count <= count - 1;
+
+        if next_count = 0 then
+          state_next <= Idle;
+        elsif (Ashift(0) = '1' and Bshift(0) = '1') then
+          state_next <= S_XNOR;
+        else
+          state_next <= S_XOR;
+        end if;
+      when S_XNOR =>
+        Carry := not (Ashift(0) xor Bshift(0));
+
+        Ashift_next <= Ashift(13) & Ashift(13 downto 1);
+        Bshift_next <= Bshift(13) & Bshift(13 downto 1);
+        SumShift_next <= Carry & SumShift(13 downto 1);
+        next_count <= count - 1;
+
+        if next_count = 0 then
+          state_next <= Idle;
+        elsif (Ashift(0) = '0' and Bshift(0) = '0') then
+          state_next <= S_XOR;
+        else
+          state_next <= S_XNOR;
+        end if;
+    end case;
+  end process;
+
+  sum <= SumShift;
+
+end architecture;
