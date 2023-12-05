788b46b254afce6d230d70d2233261eead68b1e4
Osamah Azhar <osamahazhar18@gmail.com>
2023-12-05 09:45:03 +0700

    nyoba nyoba ascii

--- ascii_bcdo.vhd
+++ ascii_bcdo.vhd
@@ -4,22 +4,31 @@ use IEEE.numeric_std.all;
 
 entity ascii_bcdo is
     port (
-        input_ascii: in std_logic_vector(31 downto 0);
-        output_bcd: out std_logic_vector(15 downto 0)
+        input_ascii_x, input_ascii_y: in std_logic_vector(31 downto 0);
+        output_bcd_x, output_bcd_y: out std_logic_vector(15 downto 0)
     );
 end ascii_bcdo;
 
 architecture behavioral of ascii_bcd is
 begin
-    process(input_ascii)
-        variable bcd_temp: std_logic_vector(15 downto 0);
+    process(input_ascii_x, input_ascii_y)
+        variable bcd_temp_x: std_logic_vector(15 downto 0);
+        variable bcd_temp_y: std_logic_vector(15 downto 0);
+
     begin
 
-        bcd_temp(15 downto 12) := input_ascii(27 downto 24);
-        bcd_temp(11 downto 8)  := input_ascii(19 downto 16);
-        bcd_temp(7 downto 4)   := input_ascii(11 downto 8);
-        bcd_temp(3 downto 0)   := input_ascii(3 downto 0);
+        bcd_temp_x(15 downto 12) := input_ascii_x(27 downto 24);
+        bcd_temp_x(11 downto 8)  := input_ascii_x(19 downto 16);
+        bcd_temp_x(7 downto 4)   := input_ascii_x(11 downto 8);
+        bcd_temp_x(3 downto 0)   := input_ascii_x(3 downto 0);
 
-        output_bcd <= bcd_temp;
+        output_bcd_x <= bcd_temp_x;
+
+        bcd_temp_y(15 downto 12) := input_ascii_y(27 downto 24);
+        bcd_temp_y(11 downto 8)  := input_ascii_y(19 downto 16);
+        bcd_temp_y(7 downto 4)   := input_ascii_y(11 downto 8);
+        bcd_temp_y(3 downto 0)   := input_ascii_y(3 downto 0);
+
+        output_bcd_y <= bcd_temp_y;
     end process;
 end behavioral;
